ea59bfecd58d49c381aa437ebacdb248c9a8dee3
Bell Eapen <github_public@gulfdoctor.net>
2020-05-25 08:32:45 -0400

    test: uses online json. remove example folder

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,7 @@
 import { FhirFormVue } from '../src/fhirform-vue';
-// import { Testq1 } from '../src/example/testq1'
-
-// describe('blah', () => {
-//   it('works', () => {
-//     expect(FhirFormVue(Testq1)).toBeDefined();
-//   });
-// });
 import fetch from 'isomorphic-fetch'
 describe('ffvue', () => {
-  it('should show id', async () => {
+  it('should output model and schema', async () => {
     await fetch('https://www.hl7.org/fhir/questionnaire-example-f201-lifelines.json')
       .then((response) => response.json())
       .then(async (myJson) => {
